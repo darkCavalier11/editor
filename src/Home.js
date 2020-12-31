78a731c83f0738f258b122b495adcc2892acab6f
sumit <sumit.pradhan65@gmail.com>
2021-01-01 00:52:22 +0530

    firebase init

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,31 @@ import React from "react";
 import "./Home.css";
 import Editor from "./Editor";
 import { useStateValue } from "./StateProvider";
-import axios from 'axios';
+import axios from "axios";
 
 function Home() {
-  const[{ lang, text }, dispatch] = useStateValue();
+  const [{ lang, text }, dispatch] = useStateValue();
   const backendUrl = "http://localhost:7000";
-  const sendText = function(e) {
+  const sendText = function (e) {
     e.preventDefault();
     axios({
       url: backendUrl,
-      method: 'POST',
+      method: "POST",
       data: {
-        data: text
-      }
-    })
-  }
+        data: text,
+        lang: lang,
+      },
+    });
+  };
   return (
     <div className="home">
       <div className="home__container">
         <div className="home__legend">
-          <h1>
-            Paste and share
-          </h1>
+          <h1>Paste and share</h1>
           <Editor className="home__editor"></Editor>
-          <button className="home__button" onClick={sendText}>GET URL</button>
+          <button className="home__button" onClick={sendText}>
+            GET URL
+          </button>
         </div>
       </div>
     </div>
